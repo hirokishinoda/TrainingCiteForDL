1c1f2f552c7f590b724f65cff8798d3f121337d8
hirokishinoda <rss23843@nifty.com>
2022-01-16 09:57:27 +0000

    [new] created temporal system.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms
+
+from model import ConvNet
+
+import base64
+
+from PIL import Image, ImageOps
+from io import BytesIO
+
 from flask import Flask
 from flask.templating import render_template
+from flask import request, jsonify
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 
 @app.route("/")
 def index():
     return render_template("index.html", message="aiueo")
+
+@app.route("/predict", methods=['POST'])
+def predict():
+
+    #if request.headers['Context-Type'] != 'application/json; charset=utf-8':
+    #    return jsonify(res='error'), 400
+
+    # request to image
+    json = request.get_json()
+    data = json['data'].replace('data:image/png;base64,', '')
+    image_binary = base64.b64decode(data)
+    image = Image.open(BytesIO(image_binary))
+
+    # background processing
+    background = Image.new("RGB", image.size, (255, 255, 255))
+    background.paste(image, mask=image.split()[3])
+
+    image = ImageOps.invert(background.convert('L')).resize((28,28))
+    image.save('sample.jpg')
+
+    # pre-processing
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    image = transform(image).unsqueeze(0)
+
+    # load model
+    model = ConvNet().cpu()
+    model.load_state_dict(torch.load("./convnet_state.pth", map_location='cpu'))
+    model.eval()
+
+    # predict
+    output = F.softmax(model(image))
+    _, prediction = torch.max(output, 1)
+
+    return jsonify({"predict":str(output), 'status':200})
 
 app.run(port=5000, debug=True)
